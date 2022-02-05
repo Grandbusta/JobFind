c845c53f83eb9ae2fa7afe82614307b4f862963d
Grandbusta <bustajay30@gmail.com>
2022-02-06 00:28:13 +0100

    job screen done

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const Jobs = [
+  {
+    name: 'Dribble',
+    logo: '',
+    title: 'Senior UI/UX Designer',
+    description: 'lorem ipsum is a good company to apply anytime anyday',
+  },
+  {
+    name: 'Facebook',
+    logo: '',
+    title: 'Senior Frontend Developer',
+    description: 'lorem ipsum is a good company to apply anytime anyday',
+  },
+  {
+    name: 'Atlassian',
+    logo: '',
+    title: 'Senior Operations manager',
+    description: 'lorem ipsum is a good company to apply anytime anyday',
+  },
+];
--- src/screens/home.js
+++ src/screens/home.js
@@ -7,15 +7,92 @@ import {
   View,
   Image,
   TouchableOpacity,
+  FlatList,
+  TextInput,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Jobs} from '../data';
 
 export default Home = () => {
+  const colors = ['#008080'];
+  const getColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{width: '100%'}}>
-        <Text>Home</Text>
-      </View>
+      <ScrollView>
+        <View style={{width: '100%', paddingHorizontal: 16}}>
+          <View style={styles.btnflex}>
+            {[{name: 'menu'}, {name: 'notifications'}].map((item, i) => (
+              <View style={styles.btnStyle}>
+                <Icon name={item.name} size={36} color={'grey'} key={i} />
+              </View>
+            ))}
+          </View>
+          <View>
+            <Text style={styles.title}>Find the worlds most Amazing job</Text>
+            <View style={styles.inputView}>
+              <TextInput
+                placeholder="Search Job"
+                style={{
+                  flex: 1,
+                  fontSize: 18,
+                }}
+              />
+              <Icon name={'search'} size={30} color={'grey'} />
+            </View>
+            <View
+              style={{
+                marginVertical: 30,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text style={{fontSize: 20, fontWeight: '700'}}>
+                Browse By Company
+              </Text>
+              <Text style={{fontWeight: '600', color: '#008080'}}>
+                See more
+              </Text>
+            </View>
+            {Jobs.map((item, i) => (
+              <View
+                style={{...styles.jobView, backgroundColor: getColor()}}
+                key={i}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{color: 'white', fontSize: 14, fontWeight: '500'}}>
+                    {item.name}
+                  </Text>
+                  <View
+                    style={{
+                      backgroundColor: 'white',
+                      padding: 4,
+                      borderRadius: 100,
+                    }}>
+                    <Icon size={16} name="bookmark" color="grey" />
+                  </View>
+                </View>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    color: 'white',
+                    fontWeight: '700',
+                    marginVertical: 15,
+                  }}>
+                  {item.title}
+                </Text>
+                <Text style={{color: 'white'}}>{item.description}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -24,27 +101,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: 'white',
   },
-  onboardImage: {
-    width: '80%',
-    height: 300,
-  },
-  onboardContent: {
-    paddingHorizontal: '10%',
-    alignItems: 'center',
-  },
   title: {
-    fontSize: 26,
+    fontSize: 30,
     fontWeight: 'bold',
-    textAlign: 'center',
+    marginVertical: '10%',
   },
   text: {
     fontSize: 16,
     textAlign: 'center',
     color: 'grey',
     marginVertical: '10%',
+  },
+  btnflex: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 15,
+  },
+  btnStyle: {
+    backgroundColor: 'whitesmoke',
+    padding: 5,
+    borderRadius: 15,
+    alignSelf: 'flex-start',
+  },
+  inputView: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'grey',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  jobView: {
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
   },
   continueButton: {
     backgroundColor: '#008080',
