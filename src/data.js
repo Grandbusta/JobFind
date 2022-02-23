39f2a292a6bdc5a125a678a762659fcb5dd6e9ef
Grandbusta <bustajay30@gmail.com>
2022-02-23 05:30:19 +0100

    job list screen done

--- src/data.js
+++ src/data.js
@@ -1,20 +1,35 @@
+const behanceLogo = require(`./images/behance.png`);
+const dribbbleLogo = require(`./images/dribbble.png`);
+const githubLogo = require(`./images/github.png`);
+const InstagramLogo = require(`./images/instagram.png`);
+
 export const Jobs = [
   {
     name: 'Dribble',
-    logo: '',
+    logo: dribbbleLogo,
     title: 'Senior UI/UX Designer',
-    description: 'lorem ipsum is a good company to apply anytime anyday',
+    description:
+      'lorem ipsum is a good company to apply anytime anyday. lorem ipsum is a good company to apply anytime anyday',
   },
   {
-    name: 'Facebook',
-    logo: '',
+    name: 'Instagram',
+    logo: InstagramLogo,
     title: 'Senior Frontend Developer',
-    description: 'lorem ipsum is a good company to apply anytime anyday',
+    description:
+      'lorem ipsum is a good company to apply anytime anyday. lorem ipsum is a good company to apply anytime anyday',
   },
   {
-    name: 'Atlassian',
-    logo: '',
+    name: 'Github',
+    logo: githubLogo,
     title: 'Senior Operations manager',
-    description: 'lorem ipsum is a good company to apply anytime anyday',
+    description:
+      'lorem ipsum is a good company to apply anytime anyday. lorem ipsum is a good company to apply anytime anyday',
+  },
+  {
+    name: 'Behance',
+    logo: behanceLogo,
+    title: 'Senior Operations manager',
+    description:
+      'lorem ipsum is a good company to apply anytime anyday. lorem ipsum is a good company to apply anytime anyday',
   },
 ];
--- src/screens/home.js
+++ src/screens/home.js
@@ -11,7 +11,12 @@ import {
   TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Jobs} from '../data';
+const behanceLogo = require(`../images/behance.png`);
+const dribbbleLogo = require(`../images/dribbble.png`);
+const githubLogo = require(`../images/github.png`);
+const InstagramLogo = require(`../images/instagram.png`);
 
 export default Home = () => {
   const colors = ['#008080'];
@@ -41,13 +46,28 @@ export default Home = () => {
               />
               <Icon name={'search'} size={30} color={'grey'} />
             </View>
+            <View style={styles.browseView}>
+              <Text style={{fontSize: 20, fontWeight: '700'}}>
+                Browse By Company
+              </Text>
+              <Text style={{fontWeight: '600', color: '#008080'}}>
+                See more
+              </Text>
+            </View>
             <View
-              style={{
-                marginVertical: 30,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              {[behanceLogo, githubLogo, InstagramLogo, dribbbleLogo].map(
+                (img, i) => (
+                  <View key={i} style={styles.companyImagesView}>
+                    <Image
+                      style={{width: '100%', height: '100%'}}
+                      source={img}
+                    />
+                  </View>
+                ),
+              )}
+            </View>
+            <View style={styles.browseView}>
               <Text style={{fontSize: 20, fontWeight: '700'}}>
                 Browse By Company
               </Text>
@@ -65,10 +85,10 @@ export default Home = () => {
                     justifyContent: 'space-between',
                     alignItems: 'center',
                   }}>
-                  <Text
-                    style={{color: 'white', fontSize: 14, fontWeight: '500'}}>
-                    {item.name}
-                  </Text>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Image style={{width: 30, height: 30}} source={item.logo} />
+                    <Text style={styles.companyName}>{item.name}</Text>
+                  </View>
                   <View
                     style={{
                       backgroundColor: 'white',
@@ -88,6 +108,21 @@ export default Home = () => {
                   {item.title}
                 </Text>
                 <Text style={{color: 'white'}}>{item.description}</Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginTop: 20,
+                  }}>
+                  <MaterialCommunity
+                    name="clock-time-three-outline"
+                    size={26}
+                    color={`white`}
+                  />
+                  <Text style={{color: 'white', marginLeft: 8}}>
+                    Be in the first 20 Applications
+                  </Text>
+                </View>
               </View>
             ))}
           </View>
@@ -145,5 +180,26 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  browseView: {
+    marginVertical: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  companyImagesView: {
+    width: 80,
+    height: 80,
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 20,
+    borderStyle: 'solid',
+    borderColor: 'lightgrey',
+  },
+  companyName: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '500',
+    marginLeft: 10,
   },
 });
